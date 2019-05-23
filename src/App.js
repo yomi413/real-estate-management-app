c930b4777aedd7c72602383ccd2c55119840a001
yomi413 <yjescano@gmail.com>
2019-05-23 18:05:15 -0400

    Added <Tenant /> component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tenant from './components/Tenant';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my App!</h1>
+      <Tenant />
     </div>
   );
 }
