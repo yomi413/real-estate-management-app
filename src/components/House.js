62f52c63965bebfa793dfc5ab6c56a6f03dbaea0
yomi413 <yjescano@gmail.com>
2019-05-23 18:06:23 -0400

    Added basic structure of <House /> and <Tenant /> components

---
+++ src/components/House.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class House extends Component {
+  render () {
+
+  }
+}
+
+export default House;
---
+++ src/components/Tenant.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Tenant extends Component {
+  render() {
+    return (
+      <div>
+        This will hold the tenant's basic information.
+        Name: 'string'
+        Lease: boolean
+        Rent: integer
+      </div>
+    )
+  }
+}
+
+export default Tenant;
