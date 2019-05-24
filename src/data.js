503a77646c9551c9c5064687be0f7d9d9af55d43
yomi413 <yjescano@gmail.com>
2019-05-24 00:25:18 -0400

    Added some basic data to work with

---
+++ src/data.js
@@ -0,0 +1,57 @@
+const tenantsInfo = [
+  {
+    name: 'John Smith',
+    apartment: 1,
+    lease: true,
+    rent: 1000
+  },
+  {
+    name: 'Michael Jackson',
+    apartment: 2,
+    lease: true,
+    rent: 1300
+  },
+  {
+    name: 'Maggie Rivera',
+    apartment: 3,
+    lease: true, 
+    rent: 1300
+  },
+  {
+    name: 'Chloe Sullivan',
+    apartment: 4,
+    lease: true,
+    rent: 1500
+  }
+]
+
+const houseInfo = [
+  {
+    apartment: 1,
+    bedrooms: 1,
+    bathrooms: 1,
+    occupied: true,
+    features: ['washer', 'dryer', 'dishwasher']
+  },
+  {
+    apartment: 2,
+    bedrooms: 2,
+    bathrooms: 1,
+    occupied: true,
+    features: ['washer', 'dryer', 'dishwasher']
+  },
+  {
+    apartment: 3,
+    bedrooms: 2,
+    bathrooms: 1,
+    occupied: true,
+    features: ['washer', 'dryer', 'dishwasher']
+  },
+  {
+    apartment: 4,
+    bedrooms: 3,
+    bathrooms: 2,
+    occupied: true,
+    features: ['washer', 'dryer', 'dishwasher']
+  }
+]
