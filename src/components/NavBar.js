96b91ad1ad4be7626a4051e49588019dcccec1d8
yomi413 <yjescano@gmail.com>
2019-05-24 14:40:03 -0400

    Added <NavBar/> component to add links to home page

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={link}
+        activeStyle={{
+          background: 'darkblue'
+        }}
+        >Home</NavLink>
+    </div>
+  )
+}
+
+export default NavBar;
