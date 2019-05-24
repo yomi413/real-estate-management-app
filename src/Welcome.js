e15969c77a15a58c6bbf2c521d20b5df4ba94f16
yomi413 <yjescano@gmail.com>
2019-05-24 14:36:46 -0400

    Added new <Welcome/> component to restructure app

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Our House App</h1>
+      </div>
+    )
+
+  }
+}
+
+export default Welcome;
