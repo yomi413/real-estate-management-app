00695e8bb334b45c8165d65b3ca3fca0406bc94a
yomi413 <yjescano@gmail.com>
2019-05-23 22:12:45 -0400

    Added additional information to be seen in the component

--- src/components/House.js
+++ src/components/House.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class House extends Component {
   render () {
-
+    return (
+      <div>
+        Picture of House
+        Address
+        Links to House Documents
+        Link TenantsList
+        <TenantsList />
+      </div>
+    )
   }
 }
 
