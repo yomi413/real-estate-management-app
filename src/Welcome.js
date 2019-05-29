e98257f3c5905b2705aede3119eacfe6534c42ee
yomi413 <yjescano@gmail.com>
2019-05-28 20:04:15 -0400

    Added a few intro paragraphs

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,9 +5,12 @@ class Welcome extends Component {
     return (
       <div>
         <h1>Our House App</h1>
+
+        <p>Recently we celebrated 13 years living in this house. In order to make life easier, I have developed this app to help us keep track of basic information as it pertains to the house.</p>
+        <p>Now we will all be able to see the same information at the same time from the comfort of our home.</p>
+        <p>Enjoy, and please provide feedback so that we can continue improving our experience.</p>
       </div>
     )
-
   }
 }
 
