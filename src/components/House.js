d15fc3f3d85a066db092e7a37ef6fd29e018bce4
yomi413 <yjescano@gmail.com>
2019-05-28 20:03:29 -0400

    Modified the image view

--- src/components/House.js
+++ src/components/House.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+// import TenantsList from '../TenantsList';
 
 class House extends Component {
   render () {
     return (
       <div>
-        Picture of House
-        Address
-        Links to House Documents
-        Link TenantsList
-        <TenantsList />
+        <img src={'https://i.imgur.com/jJyv31d.png'} alt="Our House" />
+        <p>Address: 2942 Baisley Avenue, Bronx, NY</p>
+
+        //Picture of House
+        //Address
+        //Links to House Documents
+        //Link TenantsList
+
       </div>
     )
   }
