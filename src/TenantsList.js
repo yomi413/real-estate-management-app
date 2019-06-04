7c049fea13943ee90ba77478a21d04635a14d406
yomi413 <yjescano@gmail.com>
2019-06-04 01:01:26 -0400

    Changed the location/path of the <TenantsList /> component

---
+++ src/TenantsList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import tenantsInfo from './data';
+
+class TenantsList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const tenants = tenantsInfo.map((tenant, index) => <li key={index}>{tenant.name}</li>)
+
+    return (
+      <div>
+        <h1>Current Residents</h1>
+        <ul>
+          {tenants}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TenantsList;
