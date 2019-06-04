095433a08d9a4df620b27acf24e351e048f2c56a
yomi413 <yjescano@gmail.com>
2019-06-03 20:08:34 -0400

    Added more HTML that will set up the blueprint for the application

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+// import building from '../data';
 // import TenantsList from '../TenantsList';
 
 class Building extends Component {
@@ -10,20 +12,35 @@ class Building extends Component {
   }
 
   render () {
-    // const address = "2942 Baisley Avenue, Bronx, NY"
+    const address = "2942 Baisley Avenue, Bronx, New York"
+    const description = "This is a 3-family house located in the Pelham Bay section of the Bronx, New York. The white brick facade gives the house a beautiful appearance."
+    const image = "https://i.imgur.com/jJyv31d.png"
+
     return (
       <div>
-        <center><img src={'https://i.imgur.com/jJyv31d.png'} alt="Our House" /></center>
-        <p>Address: {this.props.address}</p>
-        <h2>Documents</h2>
-        <h2>Tenants</h2>
+        <center><img src={image} alt="Our House" /></center>
+        <h2>Address</h2>
+        {address}
+        <h2>Building Description</h2>
+          <p>{description}</p>
+        <h2>Building Documents: </h2>
+          <ul>
+            <li>Mortgage Documents</li>
+            <li>Deed</li>
+            <li>Certificate of Occupancy</li>
+          </ul>
+        <h2><Link to="/tenants">Tenants</Link></h2>
+          <ul>
+            <li>Apartment 1</li>
+            <li>Apartment 2</li>
+            <li>Apartment 3</li>
+          </ul>
         <h2>Important Links and Information</h2>
-
-        //Picture of House
-        //Address
-        //Links to House Documents
-        //Link TenantsList
-
+          <ul>
+            <li>Ocwen</li>
+            <li>NYC Department of Finance</li>
+            <li>NYC Department of Buildings</li>
+          </ul>
       </div>
     )
   }
