bc1a9d53a23e37f76e8288f6c3c7f4feaba2da09
yomi413 <yjescano@gmail.com>
2019-06-05 09:16:57 -0400

    Added chart style to create the rent roll

--- src/TenantsList.js
+++ src/TenantsList.js
@@ -4,14 +4,36 @@ import tenantsInfo from './data';
 class TenantsList extends Component {
 
   render() {
-    const tenants = tenantsInfo.map((tenant, index) => <li key={index}>{tenant.name}</li>)
+    const tenants = tenantsInfo.map((tenant, index) =>
+      <tr>
+        <td>{tenant.apartment}</td>
+        <td>{tenant.name}</td>
+        <td>${tenant.rent}</td>
+        <td>{tenant.leaseCommencement}</td>
+        <td>{tenant.leaseExpiration}</td>
+      </tr>)
+    // const tenants = tenantsInfo.map((tenant, index) => <li key={index}>{tenant.name}</li>)
 
     return (
       <div>
         <h1>Current Residents</h1>
-        <ul>
-          {tenants}
-        </ul>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Apartment Number</th>
+              <th>Tenant Name</th>
+              <th>Rent Amount</th>
+              <th>Lease Commencement</th>
+              <th>Lease Expiration</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {tenants}
+          </tbody>
+        </table>
+
       </div>
     )
   }
