5c880f9822a47096237cd3337f0eb12da3c4f74c
yomi413 <yjescano@gmail.com>
2019-06-06 18:18:12 -0400

    Added code to use a pdf viewer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
 import './App.css';
-// import Welcome from '/Welcome';
-import Tenant from './components/Tenant';
-// import House from './components/House';
-// import TenantsList from 'TenantsList';
+import PDFViewer from './components/PDFViewer/PDFViewer';
+import PDFJs from './backends/pdfjs';
 
-function App() {
-  return (
-    <div className="App">
-
-      <Tenant name="Tom" lease='true' rent='1000'/>
-
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PDFViewer
+          backend={PDFJs}
+          src='/public/deed.pdf'
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//
+//       <Tenant name="Tom" lease='true' rent='1000'/>
+//
+//
+//     </div>
+//   );
+// }
+//
+// export default App;
