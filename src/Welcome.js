df147cf784bc24b3251c3e2bfa0991b1baeee133
yomi413 <yjescano@gmail.com>
2019-06-08 08:06:02 -0400

    Provided a different design for the home page

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Welcome extends Component {
   render() {
-    const url = window.location.pathname
 
     return (
       <div>
-        <h1>Our House App</h1>
-
-        <p>Recently we celebrated 13 years living in this house. In order to make life easier, I have developed this app to help us keep track of basic information as it pertains to the house.</p>
-        <p>Now we will all be able to see the same information at the same time from the comfort of our home.</p>
-        <p>Enjoy, and please provide feedback so that we can continue improving our experience.</p>
+        <center><h1>Escano Real Estate Services</h1></center>
+        <img src={"https://i.imgur.com/lpvbSRT.jpg"} />
+        <button><Link to='/building/new'>New Building</Link></button>
+        <button><Link to="/buildings">Existing Building</Link> </button>
       </div>
     )
   }
