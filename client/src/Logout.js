49d551fbe1ac0b4cb236b061d516456901bc8e43
yomi413 <yjescano@gmail.com>
2019-06-13 10:04:54 -0400

    Added new <Logout /> component

---
+++ client/src/Logout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>You have logged out successfully.</h1>
+      </div>
+    )
+  }
+}
+
+export default Logout;
