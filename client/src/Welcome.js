7f7eb0b9e8202c26c9d4fb07dfa888785eaffc07
yomi413 <yjescano@gmail.com>
2019-06-14 21:25:34 -0400

    Modified the <Welcome /> to make it more 'welcoming'

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -7,10 +7,10 @@ class Welcome extends Component {
 
     return (
       <div>
+        <NavBar />
         <center>
-
-          <h1>Escano Real Estate Services</h1>
-          <img src={"https://i.imgur.com/lpvbSRT.jpg"} className=""/>
+          <h1 className="homeHeader">Escano Real Estate Services</h1>
+          <img className="skyscrapers" src={"https://i.imgur.com/lpvbSRT.jpg"} className=""/>
           <button><Link to='/building/new'>New Building</Link></button>
           <button><Link to="/buildings">Existing Building</Link> </button>
         </center>
