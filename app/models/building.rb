7356abc0ea17eda5b5464a728bbf9766bf79f827
yomi413 <yjescano@gmail.com>
2019-06-14 20:38:12 -0400

    Added validations to the building model

---
+++ app/models/building.rb
@@ -0,0 +1,7 @@
+class Building < ApplicationRecord
+  belongs_to :user
+  
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :numberOfApartments, presence: true
+end
