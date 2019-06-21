6676cca7e3fba0cfeffccf428fae86bd9bf11abe
yomi413 <yjescano@gmail.com>
2019-06-21 00:51:16 -0400

    Modified the sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :users
 
   get '/session', to: 'sessions#new'
+
   post '/session', to: 'sessions#login', as: 'login'
-  delete '/session', to: 'sessions#destroy'
+  
+  delete '/session', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
