dc4dbbf8b93b2c7b30c3447b8037544af2f45737
yomi413 <yjescano@gmail.com>
2019-06-21 01:00:42 -0400

    Modified the links included through the <NavBar />

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import NavBar from './components/NavBar';
 
 class Welcome extends Component {
@@ -11,8 +11,7 @@ class Welcome extends Component {
         <center>
           <h1 className="homeHeader">Escano Real Estate Services</h1>
           <img className="skyscrapers" src={"https://i.imgur.com/lpvbSRT.jpg"} className=""/>
-          <button><Link to='/building/new'>New Building</Link></button>
-          <button><Link to="/buildings">Existing Building</Link> </button>
+
         </center>
       </div>
     )
