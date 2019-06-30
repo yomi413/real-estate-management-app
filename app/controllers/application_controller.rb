a00bd2391c47a14f606b8c7114fe2bc6ca5188dc
yomi413 <yjescano@gmail.com>
2019-06-29 22:55:46 -0400

    Added some previously used helper methods into the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::API
   self.responder = ApplicationResponder
   respond_to :json
 
+  # protect_from_forgery with: :exception
+  # helper_method :logged_in?, :current_user
+
+  def verified_user
+    redirect_to '/' unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id:session[:user_id])
+  end
+
 end
