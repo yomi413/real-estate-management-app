b4f106b542f7475052499e463a82aa97fe31dc98
yomi413 <yjescano@gmail.com>
2019-06-29 22:49:33 -0400

    Modified the <Buildings /> component to render the list of buildings correctly

--- client/src/components/Buildings.js
+++ client/src/components/Buildings.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 
 class Buildings extends Component {
-  render() {
-    const buildingsList = [
-      "2942 Baisley Avenue, Bronx, NY",
-      "1373 Ellison Avenue, Bronx, NY",
-      "2940 Baisley Avenue, Bronx, NY",
-    ]
+  state = {
+    buildings: []
+  }
 
-    const buildings = buildingsList.map((building, index) => <li key={index}>{building}</li>)
+  componentDidMount() {
+    fetch('http://localhost:3001/buildings.json')
+    .then(response => response.json())
+    .then(data => {
+      // debugger
+      this.setState({
+        buildings: data.buildings
+      })
+    })
+    .catch(error => {
+      debugger
+    })
+  }
+
+  render() {
+
+    const buildingsList = this.state.buildings.map(building => {
+      return <li>{building.address}</li>
+    })
 
     return (
       <div>
-        <h1>All Buildings</h1>
-        {buildings}
+        <h1>Buildings</h1>
+        {buildingsList}
       </div>
     )
+    // const buildingsList = [
+    //   "2942 Baisley Avenue, Bronx, NY",
+    //   "1373 Ellison Avenue, Bronx, NY",
+    //   "2940 Baisley Avenue, Bronx, NY",
+    // ]
+    //
+    // const buildings = buildingsList.map((building, index) => <li key={index}>{building}</li>)
+    //
+    // return (
+    //   <div>
+    //     <h1>All Buildings</h1>
+    //     {buildings}
+    //   </div>
+    // )
   }
 }
 
