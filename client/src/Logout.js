05d31ad2b12182bd24311baac957d42683266f79
yomi413 <yjescano@gmail.com>
2019-06-29 22:51:13 -0400

    Modified the <Logout /> component, but will need to revisit

--- client/src/Logout.js
+++ client/src/Logout.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
 class Logout extends Component {
-
+  handleLogout = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3001/session', {
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then((response) => {
+      return Promise.all([response.ok, response.json()]);
+    }).then(([ok, json]) => {
+      if (ok) {
+        this.props.history.push('/user-welcome')
+      } else {
+        this.setState({ error: json['errors'] })
+      }
+    })
+  }
 
   render() {
     return (
-      <div>
-        <h1>You have logged out successfully.</h1>
-      </div>
+
+      <button type="button" onClick={this.handleLogout}>Logout Button</button>
+
     )
   }
+
 }
 
-export default Logout;
+export default withRouter(Logout);
