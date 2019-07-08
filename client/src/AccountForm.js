0e0e897437a4778f6e14f79fa5442ba5a52a4397
github username <github email address>
2019-07-08 14:21:44 -0400

    Modified the <AccountForm /> to add spaces and navigation capabilities

--- client/src/AccountForm.js
+++ client/src/AccountForm.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
 
-const AccountForm = (props) => (
+const AccountForm = props => (
   <div>
-    <div style={{color: 'red'}}>{props.error}</div>
+    <NavBar />
+    <div style={{ color: "red" }}>{props.error}</div>
     <form onSubmit={props.onSubmit}>
       <h1>{props.heading}</h1>
       <div>
         <label htmlFor="email">Email: </label>
-        <input type="text" name="email" placeholder="Email" onChange={props.onChange}/>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={props.onChange}
+        />
       </div>
-
+      <br />
       <div>
         <label htmlFor="password">Password: </label>
-        <input type="password" name="password" placeholder="Password" onChange={props.onChange}/>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={props.onChange}
+        />
       </div>
-
+      <br />
       <input type="submit" value="Submit" />
     </form>
   </div>
