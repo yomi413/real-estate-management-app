a29084bb3de3914776de0008c5616a9c29313a48
github username <github email address>
2019-07-08 21:47:19 -0400

    Updated the schema with the creation of the sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_123657) do
+ActiveRecord::Schema.define(version: 2019_07_08_193810) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "address"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_06_14_123657) do
     t.string "numberOfApartments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "uid"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
