6ed664db9857851162bb74f9d1eaf52a67d5ec44
github username <github email address>
2019-07-08 21:59:28 -0400

    Added sessions code to localStorage object

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
-import AccountForm from './AccountForm';
+import React, { Component } from "react";
+import AccountForm from "./AccountForm";
 
 class Login extends Component {
   state = {
-    email: '',
-    password: '',
-    error: ''
-  }
+    email: "",
+    password: "",
+    error: ""
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
-    fetch('http://localhost:3001/session', {
-      credentials: 'include',
-      method: 'POST',
+    fetch("http://localhost:3001/session", {
+      credentials: "include",
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json"
       },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password
       })
     })
-    .then((response) => {
-      return Promise.all([response.ok, response.json()]);
-    }).then(([ok, json]) => {
-      if (ok) {
-        this.props.history.push('/user-welcome')
-      } else {
-        this.setState({ error: json['errors'] })
-      }
-    })
-  }
+      .then(response => {
+        return Promise.all([response.ok, response.json()]);
+      })
+      .then(([ok, json]) => {
+        if (ok) {
+          localStorage["json.sessionUid"] = json.sessionUid;
+          this.props.history.push("/user-welcome");
+        } else {
+          this.setState({ error: json["errors"] });
+        }
+      });
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -47,7 +49,7 @@ class Login extends Component {
         onSubmit={this.handleSubmit}
         onChange={this.handleChange}
       />
-    )
+    );
   }
 }
 
