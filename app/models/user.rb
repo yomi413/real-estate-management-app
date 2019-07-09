47a0295545c8fb6b1dfa3abfb5be7a046daa2ddd
github username <github email address>
2019-07-08 22:15:27 -0400

    Added new sessions association s

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
+require 'securerandom'
+
 class User < ApplicationRecord
   has_many :buildings
+  has_many :sessions
 
   has_secure_password(validations: false)
 
   validates :email, presence: true
   validates :password, presence: true
+
+  # accepts_nested_attributes_for :building 
 end
