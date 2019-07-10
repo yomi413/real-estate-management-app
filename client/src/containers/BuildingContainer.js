c635b3f4642404d25dc48e9c0ea1abf1fbb96a36
github username <github email address>
2019-07-10 14:19:48 -0400

    Added uid to the body

--- client/src/containers/BuildingContainer.js
+++ client/src/containers/BuildingContainer.js
@@ -1,65 +1,81 @@
-import React, { Component } from 'react';
-import NavBar from '../components/NavBar';
+import React, { Component } from "react";
+import NavBar from "../components/NavBar";
 
 class BuildingContainer extends Component {
   state = {
-    address: '',
-    description: '',
-    numberOfApartments: ''
-  }
+    address: "",
+    description: "",
+    numberOfApartments: ""
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
-    fetch('http://localhost:3001/buildings', {
-      method: 'POST',
+    fetch("http://localhost:3001/buildings", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json"
       },
       body: JSON.stringify({
+        uid: localStorage["json.sessionUid"],
         address: this.state.address,
         description: this.state.description,
         numberOfApartments: this.state.numberOfApartments
       })
-    })
-    .then(() => {
-      this.props.history.push('/buildings')
-    })
-  }
+    }).then(() => {
+      this.props.history.push("/buildings");
+    });
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   render() {
     return (
-      <div><NavBar />
+      <div>
+        <NavBar />
         <form onSubmit={this.handleSubmit}>
           <h1>New Building</h1>
           <div>
             <label htmlFor="address">Address: </label>
-            <input type="text" name="address" placeholder="Address" onChange={this.handleChange} />
+            <input
+              type="text"
+              name="address"
+              placeholder="Address"
+              onChange={this.handleChange}
+            />
           </div>
-          <br></br>
+          <br />
 
           <div>
             <label htmlFor="description">Description: </label>
-            <input type="text" name="description" placeholder="Description" onChange={this.handleChange} />
+            <input
+              type="text"
+              name="description"
+              placeholder="Description"
+              onChange={this.handleChange}
+            />
           </div>
-          <br></br>
+          <br />
 
           <div>
             <label htmlFor="numberOfApartments">Number of Apartments: </label>
-            <input type="number" name="numberOfApartments" placeholder="# of Apartments" onChange={this.handleChange} />
+            <input
+              type="number"
+              name="numberOfApartments"
+              placeholder="# of Apartments"
+              onChange={this.handleChange}
+            />
           </div>
-          <br></br>
+          <br />
 
           <input type="submit" value="Submit" />
         </form>
       </div>
-    )
+    );
   }
 }
 
