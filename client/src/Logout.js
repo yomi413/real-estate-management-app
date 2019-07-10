d22e15df4ed61ca56f4e9938427a8926beb41df3
github username <github email address>
2019-07-10 12:43:39 -0400

    Modified the <Logout /> component to account for proper deletion of the current session

--- client/src/Logout.js
+++ client/src/Logout.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, withRouter } from "react-router-dom";
 
 class Logout extends Component {
-  handleLogout() {
-    localStorage.clear();
-    window.location.href = '/';
-  }
+  handleLogout = () => {
+    fetch("http://localhost:3001/session", {
+      credentials: "include",
+      method: "DELETE",
+      body: JSON.stringify({
+        uid: localStorage["json.sessionUid"]
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+      .then(response => {
+        return Promise.all([response.ok, response.json()]);
+      })
+      .then(([ok, json]) => {
+        if (ok) {
+          localStorage.clear(json.sessionUid);
+          // window.location.href = "/";
+        } else {
+          this.setState({ error: json["errors"] });
+        }
+      });
+  };
 
   render() {
     return (
       <div>
         <h3>Are you sure you want to log out?</h3>
-        <button type="button" onClick={this.handleLogout}>Logout Button</button>
+        <button type="button" onClick={this.handleLogout}>
+          Logout Button
+        </button>
       </div>
-    )
+    );
   }
 }
 
