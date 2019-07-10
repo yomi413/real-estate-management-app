1f8933465d8438240854bfafd0df6768223cadd0
github username <github email address>
2019-07-10 14:22:23 -0400

    Cleaned up the componentDidMount()

--- client/src/components/Buildings.js
+++ client/src/components/Buildings.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
 
 class Buildings extends Component {
   state = {
     buildings: []
-  }
+  };
 
   componentDidMount() {
-    fetch('http://localhost:3001/buildings.json')
-    .then(response => response.json())
-    .then(data => {
-      // debugger
-      this.setState({
-        buildings: data.buildings
+    fetch("http://localhost:3001/buildings.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          buildings: data.buildings
+        });
       })
-    })
-    .catch(error => {
-      debugger
-    })
+      .catch(error => {});
   }
 
   render() {
-
     const buildingsList = this.state.buildings.map(building => {
-      return <li>{building.address}</li>
-    })
+      return <li key={building}>{building.address}</li>;
+    });
 
     return (
-      
       <div>
         <NavBar />
         <h1>Buildings</h1>
         {buildingsList}
       </div>
-    )
+    );
     // const buildingsList = [
     //   "2942 Baisley Avenue, Bronx, NY",
     //   "1373 Ellison Avenue, Bronx, NY",
