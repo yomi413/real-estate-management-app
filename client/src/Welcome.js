4b47ef94db83d5de2d66f02a878b764d893fa410
github username <github email address>
2019-07-12 11:52:31 -0400

    Modfied the <Welcome /> page with more aesthetics

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar';
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import FooterComponent from "./components/FooterComponent";
 
 class Welcome extends Component {
   render() {
-
     return (
-      <div>
-        <NavBar />
-        <center>
-          <h1 className="homeHeader">Escano Real Estate Services</h1>
-          <img className="skyscrapers" src={"https://i.imgur.com/lpvbSRT.jpg"} className=""/>
-        </center>
-      </div>
-    )
+      <React.Fragment>
+        <div>
+          <NavBar />
+          <center>
+            <h1 className="homeHeader">Escano/Asis Real Estate</h1>
+            <h5>
+              Escano/Asis Real Estate is a service provided so that we may all
+              be able to view the information available on our property at the
+              same time.
+            </h5>
+            <img
+              className="skyscrapers"
+              src={"https://i.imgur.com/lpvbSRT.jpg"}
+            />
+          </center>
+        </div>
+        <FooterComponent />
+      </React.Fragment>
+    );
   }
 }
 
