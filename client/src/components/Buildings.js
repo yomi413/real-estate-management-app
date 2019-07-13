31975bd5a4ee83037a8fee04514f1afefc6b7f6f
github username <github email address>
2019-07-12 23:06:31 -0400

    Modified <Buildings /> component to properly show each user's list of buildings

--- client/src/components/Buildings.js
+++ client/src/components/Buildings.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
+
+const fetchBuildings = () => {
+  return dispatch => {
+    fetch(
+      `http://localhost:3001/buildings.json?uid=${
+        localStorage["json.sessionUid"]
+      }`
+    )
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: "ADD_BUILDINGS", buildings: data.buildings });
+      });
+  };
+};
 
 class Buildings extends Component {
   state = {
@@ -7,43 +23,34 @@ class Buildings extends Component {
   };
 
   componentDidMount() {
-    fetch("http://localhost:3001/buildings.json")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          buildings: data.buildings
-        });
-      })
-      .catch(error => {});
+    this.props.fetchBuildings();
   }
 
   render() {
-    const buildingsList = this.state.buildings.map(building => {
-      return <li key={building}>{building.address}</li>;
+    const buildingsList = this.props.buildings.map((building, index) => {
+      return (
+        <li key={index}>
+          <a href={`building/${building.id}`}>{building.address}</a>
+        </li>
+      );
     });
 
     return (
       <div>
-        <NavBar />
         <h1>Buildings</h1>
-        {buildingsList}
+        <ul>{buildingsList}</ul>
       </div>
     );
-    // const buildingsList = [
-    //   "2942 Baisley Avenue, Bronx, NY",
-    //   "1373 Ellison Avenue, Bronx, NY",
-    //   "2940 Baisley Avenue, Bronx, NY",
-    // ]
-    //
-    // const buildings = buildingsList.map((building, index) => <li key={index}>{building}</li>)
-    //
-    // return (
-    //   <div>
-    //     <h1>All Buildings</h1>
-    //     {buildings}
-    //   </div>
-    // )
   }
 }
 
-export default Buildings;
+const mapDispatchToProps = { fetchBuildings };
+
+const mapStateToProps = state => ({
+  buildings: state.buildings
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Buildings);
