6315be9140625d6a0e7084028920f8c3e7ba1cc4
github username <github email address>
2019-07-12 23:03:56 -0400

    Modified the <BuildingDocuments /> component to assist in presenting proper building information

--- client/src/BuildingDocuments.js
+++ client/src/BuildingDocuments.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
-import App from './App';
-import BuildingContainer from './containers/BuildingContainer';
+import React, { Component } from "react";
+import App from "./App";
+import BuildingContainer from "./containers/BuildingContainer";
 // import { Document } from 'react-pdf';
 
 class BuildingDocuments extends Component {
   render() {
-    const mortgage = ""
-    const deed = ""
-    const cofo = ""
-
     return (
-
       <div>
-        <h1>House Documents</h1>
+        <h2>Building Documents</h2>
         <ul>
-          <li>Mortgage Documents</li>
-          <li><App /></li>
-          <li>Certificate of Occupancy</li>
+          <li>
+            <a href="https://a836-acris.nyc.gov/DS/DocumentSearch/DocumentImageView?doc_id=2006060201497001">
+              Deed
+            </a>
+          </li>
+
+          <li>
+            <a href="https://a836-acris.nyc.gov/DS/DocumentSearch/DocumentImageView?doc_id=2006060201497002">
+              PHH Mortgage
+            </a>
+          </li>
+
+          <li>
+            <a href="https://a836-acris.nyc.gov/DS/DocumentSearch/DocumentImageView?doc_id=2006060201688001">
+              Real Time Solutions Mortgage
+            </a>
+          </li>
+
+          <li>
+            <a href="https://a836-acris.nyc.gov/DS/DocumentSearch/DocumentImageView?doc_id=2016101200971001">
+              Real Time Solutions Satisfaction of Mortgage
+            </a>
+          </li>
+
+          <li>
+            <a href="chrome-extension://oemmndcbldboiebfnladdacbdfmadadm/file:///Users/yomairaescano/Downloads/200861825T1%20(1).PDF">
+              Temporary Certificate of Occupancy
+            </a>
+          </li>
+
+          <li>
+            <a href="chrome-extension://oemmndcbldboiebfnladdacbdfmadadm/file:///Users/yomairaescano/Downloads/200861825F.PDF">
+              Final Certificate of Occupancy
+            </a>
+          </li>
         </ul>
-      <BuildingContainer />
       </div>
-    )
+    );
   }
 }
 
