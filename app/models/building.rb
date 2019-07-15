a52426ec3fd0762512598add26c07ba6f1aaba44
github username <github email address>
2019-07-15 19:02:34 -0400

    Modified the association to the documents database and added the nested attributes for document

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,10 @@
 class Building < ApplicationRecord
   belongs_to :user
+  has_one :document
 
   validates :address, presence: true
   validates :description, presence: true
   validates :numberOfApartments, presence: true
+
+  accepts_nested_attributes_for :document
 end
