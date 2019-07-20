f264000e89c13765d77e323c314819440fa49e38
github username <github email address>
2019-07-20 08:36:08 -0400

    Changed the validations for all attributes

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ApplicationRecord
     belongs_to :building
+
+    # validates :deed, presence: true
+    # validates :mortgage_1, presence: true
+    # validates :mortgage_2, presence: true
+    # validates :satisfaction_of_mortgage_1, presence: true
+    # validates :satisfaction_of_mortgage_2, presence: true
+    # validates :certificate_of_occupancy, presence: true
 end
