1c576df8dded297339a2807564ceb46d296ac57c
github username <github email address>
2019-07-20 13:27:46 -0400

    Clean up of code

--- client/src/components/FooterComponent.jsx
+++ client/src/components/FooterComponent.jsx
@@ -5,8 +5,6 @@ class FooterComponent extends Component {
     return (
       <div className="footer-container ">
         <footer className="footer mt-auto py-3">
-          {/* <div className="container footer_block noBotBorder me-20" /> */}
-
           <div className="row">
             <div className="col-md-6">
               <h5 className="footer-hLine">Escano/Asis Real Estate Services</h5>
@@ -24,12 +22,12 @@ class FooterComponent extends Component {
               <div id="map">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3018.630289051182!2d-73.82819549900489!3d40.83608370340723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c28b466e54e345%3A0x4d1d7a4ed51dc1d4!2s2942+Baisley+Ave%2C+The+Bronx%2C+NY+10461!5e0!3m2!1sen!2sus!4v1562894073893!5m2!1sen!2sus"
-                  width="100%"
-                  height="225"
-                  frameBorder="50"
-                  scrolling="no"
-                  marginHeight="0"
-                  marginWidth="0"
+                  // width="100%"
+                  // height="225"
+                  // frameBorder="50"
+                  // scrolling="no"
+                  // marginHeight="0"
+                  // marginWidth="0"
                 />
               </div>
             </div>
