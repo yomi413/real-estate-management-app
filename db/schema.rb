807730e2abf1c787c0d0e0f59312380c52915080
github username <github email address>
2019-07-20 08:28:14 -0400

    Adding the columns to the users table updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_024710) do
+ActiveRecord::Schema.define(version: 2019_07_20_033644) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "address"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2019_07_13_024710) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
