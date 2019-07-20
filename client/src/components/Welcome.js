ddfc955523e77e781970e8cf730c6e822889e581
github username <github email address>
2019-07-20 13:16:08 -0400

    Added the <Welcome /> to the components folder

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import FooterComponent from "./FooterComponent";
+
+const Welcome = () => (
+  <React.Fragment>
+    <div>
+      <NavBar />
+      <center>
+        <h1 className="homeHeader">Escano/Asis Real Estate</h1>
+        <h5>
+          Escano/Asis Real Estate is a site designed and developed with the
+          intention of providing a one-stop-shop for our group's real estate.
+          This site will help keep us organized and ready to tackle the needs of
+          our properties.
+        </h5>
+        <img className="skyscrapers" src={"https://i.imgur.com/lpvbSRT.jpg"} />
+      </center>
+    </div>
+    <FooterComponent />
+  </React.Fragment>
+);
+
+export default Welcome;
