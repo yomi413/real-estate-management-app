66e9682cd76e0ffb1854c20a480a9f94d7fedf66
github username <github email address>
2019-07-20 13:25:04 -0400

    Added <BuildingFactsComponent /> to the components folder

---
+++ client/src/components/BuildingFactsComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+const BuildingFactsComponent = props => (
+  <div>
+    <ul>
+      <li>
+        <h6>Address</h6>
+        {props.address}
+      </li>
+
+      <li>
+        <h6>Building Description</h6>
+        {props.description}
+      </li>
+
+      <li>
+        <h6>Number of Apartments</h6>
+        {props.numberOfApartments}
+      </li>
+    </ul>
+  </div>
+);
+
+export default BuildingFactsComponent;
