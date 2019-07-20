1a66ae31674a2edc1c33cebbd60cdc0c5eabb1f1
github username <github email address>
2019-07-20 13:25:42 -0400

    Add <Contacts /> to the components folder

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+const Contacts = () => (
+  <div>
+    <h3>Important Links</h3>
+    <ul>
+      <li>
+        <a href="http://www.ocwen.com/ocwencustomers">
+          PHH Mortgage Corporation
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www1.nyc.gov/site/finance/index.page">
+          NYC Department of Finance
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www1.nyc.gov/site/buildings/index.page">
+          NYC Department of Buildings
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www.zillow.com/homes/2942-Baisley-Avenue,-Bronx,-NY-10461_rb/83178696_zpid/">
+          Zillow
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Contacts;
