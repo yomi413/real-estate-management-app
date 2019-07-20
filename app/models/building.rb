949c0046ab602b646d2cd7cb0fb29a16949698e6
github username <github email address>
2019-07-20 10:40:36 -0400

    Modified the validations

--- app/models/building.rb
+++ app/models/building.rb
@@ -4,6 +4,7 @@ class Building < ApplicationRecord
 
   validates :address, presence: true
   validates :description, presence: true
+  # validates :image, presence: true
   validates :numberOfApartments, presence: true
 
   accepts_nested_attributes_for :document
