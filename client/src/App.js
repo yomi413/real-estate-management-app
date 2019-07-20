5eee250e34ea975dde179cd8c1406eb74580e133
github username <github email address>
2019-07-20 13:01:26 -0400

    Cleanup of this component (though it is not being used

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,10 @@
-import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-import './App.css';
-import PDFViewer from './components/PDFViewer/PDFViewer';
-import PDFJs from './backends/pdfjs';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <PDFViewer
-          backend={PDFJs}
-          src='/public/deed.pdf'
-        />
-      </div>
-    )
+    return <div className="App" />;
   }
 }
 
 export default App;
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//
-//       <Tenant name="Tom" lease='true' rent='1000'/>
-//
-//
-//     </div>
-//   );
-// }
-//
-// export default App;
