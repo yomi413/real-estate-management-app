76850f693700d6e2a00a8e6762e03c22cd789128
github username <github email address>
2019-07-21 20:36:54 -0400

    Add the update and edit actions to the buildings controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -29,6 +29,34 @@ class BuildingsController < ApplicationController
     render json: {building: building} 
   end
 
+  def edit
+    session = Session.find_by(uid: params[:uid])
+    building = session.user.buildings.find_by(id: params[:id])
+  end
+
+  def update
+    session = Session.find_by(uid: params[:uid])
+    building = session.user.buildings.find_by(id: params[:id])
+    # binding.pry
+    if building && building.update(building_params)
+      render json: {building: building}, status: 200
+    else
+      render json: {errors: "Unable to update"}, status: 422
+    end
+  end
+
+  def destroy
+    session = Session.find_by(uid: params[:uid])
+    building = session.user.buildings.find_by(id: params[:id])
+
+    if building
+      building.destroy()
+      render json: {success: true}, status: 200
+    else
+      render json: {errors: "Unable to delete this item."}, state: 422
+    end
+  end
+
   private
 
   def building_params
