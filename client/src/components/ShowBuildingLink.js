c3c2e2a1365912d52598252df8dda5de5db238da
github username <github email address>
2019-07-21 23:15:41 -0400

    New component to show the list of buildings

---
+++ client/src/components/ShowBuildingLink.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { deleteBuilding } from "../actions";
+
+class ShowBuildingLink extends Component {
+  render() {
+    const { building } = this.props;
+
+    return (
+      <tr>
+        <td>
+          <Link to={`building/${building.id}`}>{building.address}</Link>
+        </td>
+        <td>
+          <Link
+            className="btn btn-primary btn-sm"
+            to={`building/${building.id}/edit`}
+          >
+            Edit
+          </Link>
+        </td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-primary btn-sm"
+            onClick={() => this.props.deleteBuilding(this.props.building.id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+const mapDispatchToProps = { deleteBuilding };
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShowBuildingLink);
